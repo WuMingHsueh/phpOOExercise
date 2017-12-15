87067352e8e046d1e61007cd4de5f7b4ea5f24de
Wu Ming-Hsueh <ri3567@gmail.com>
2017-12-15 17:45:22 +0800

    before class extend

--- app/Entree.php
+++ app/Entree.php
@@ -4,5 +4,40 @@ namespace OOExercise;
 
 class Entree
 {
+    public $name;
+    public $ingredients = [];
+    public $dinner = "披薩";
+    private $lunch = "鍋貼";
+
+    public function _construct($name, array $ingredients)
+    {
+        $this->name = $name;
+        $this->ingredients = $ingredients;
+    }
     
+    public function hasIngredient($ingredient)
+    {
+        return in_array($ingredient,  $this->ingredients);
+    }
+
+    public function hasDinner($price = null)
+    {
+        echo "原本吃 : " . $this->dinner . "<br>" ;
+        if (!is_null($price))
+            $this->dinner .= ",海膽,魚翅";
+        echo "加碼吃 : " . $this->dinner . "<br>";
+    }
+
+    public function hasLunch($price = null)
+    {
+        echo "原本吃 : " . $this->lunch . "<br>";
+        if (!is_null($price))
+            $this->lunch .= ",酸辣湯";
+    echo "加碼吃 : " . $this->lunch . "<br>";
+    }
+
+    public static function getSize()
+    {
+        return ["small", "medium", "large"];
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
 include __DIR__ . "./vendor/autoload.php";
+
+use OOExercise\Entree as Entree;
+
+
+$soup = new Entree;
+$soup->name = 'Chicken Soup';
+$soup->ingredients = ['chicken', 'water'];
+
+$sandwich = new Entree;
+$sandwich->name = 'Chicken Sandwich';
+$sandwich->ingredients = ['chicken', 'bread'];
+
+foreach (['chicken', 'lemon', 'bread', 'water'] as $ing) {
+    if ($soup->hasIngredient($ing))
+        print "Soup contains $ing. <br>";
+    
+    if ($sandwich->hasIngredient($ing))
+        print "Sandwich contains $ing. <br>";
+}
